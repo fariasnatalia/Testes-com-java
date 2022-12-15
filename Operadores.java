e3c2eb852dc8ddf54f990c57dab15ed33c553c39
Natália Farias <nataliafsoficial@gmail.com>
2022-12-15 18:13:56 -0300

    add

---
+++ Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores { 
+
+    /** 
+    
+    * @param args 
+    
+    */ 
+    
+    public static void main(String[] args) { 
+    
+    int numero = 5; 
+   
+    numero = numero - 1; //subitraindo o valor da variável 
+    
+    System.out.println(numero); 
+    
+    numero = numero + 1; 
+    
+    System.out.println(numero);//atribuindo valor a variável 
+} 
+    
+     
+     
+    
+    } 
---
+++ README.md
@@ -0,0 +1,3 @@
+# Atividades realizadas durante o curso java-dio
+
+![código](https://img.freepik.com/fotos-premium/desenvolvedor-de-software-java-programando-codigo-web-html-codigo-de-script-de-computador-abstrato-tela-de-codigo-de-programacao-do-desenvolvedor-de-software-tempo-de-trabalho-da-programacao-de-software_263512-1380.jpg?w=1380)
